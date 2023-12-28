a6be4689f74d4d5913324057f8ce6b6227605df2
Chibunud <ndubest56@gmail.com>
2023-12-29 00:35:04 +0100

    header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
-
 import './App.css'
-import { LogoIcon } from './assets/Icons'
+import { LogoIcon, SearchIcon } from './assets/Icons'
 
 function App() {
   return (
     <div>
-      <header>
-        <LogoIcon />
-        <span>Lifella</span>
+      <header className='p-4 flex justify-between'>
+        <a href='#' className='flex gap-1 items-center'>
+          <LogoIcon />
+          <span className='text-lg font-bold'>lifella</span>
+        </a>
+        <div className='flex gap-2 border border-gray-300 rounded-full p-2 px-4 shadow-md shadow-gray-300 '>
+          <div>Any Where</div>
+          <div className='border-l border-gray-300 '></div>
+          <div>Any Time</div>
+          <div className='border-l border-gray-300 '></div>
+          <div>Any One</div>
+          <SearchIcon />
+        </div>
       </header>
     </div>
   )
