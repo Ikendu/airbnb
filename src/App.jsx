ca1205d684a29b70bf9b70b5d9aaf2b6de8239ba
Chibunud <ndubest56@gmail.com>
2023-12-29 01:27:23 +0100

    header component design

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { LogoIcon, SearchIcon } from './assets/Icons'
+import { BarIcon, GlobeIcon, LogoIcon, SearchIcon, UserIcon } from './assets/Icons'
 
 function App() {
   return (
@@ -9,13 +9,26 @@ function App() {
           <LogoIcon />
           <span className='text-lg font-bold'>lifella</span>
         </a>
-        <div className='flex gap-2 border border-gray-300 rounded-full p-2 px-4 shadow-md shadow-gray-300 '>
-          <div>Any Where</div>
-          <div className='border-l border-gray-300 '></div>
-          <div>Any Time</div>
-          <div className='border-l border-gray-300 '></div>
-          <div>Any One</div>
-          <SearchIcon />
+        <div className='flex justify-between items-center'>
+          <div className='flex gap-2 border border-gray-300 rounded-full p-2 px-4 shadow-md shadow-gray-300 '>
+            <div>Any Where</div>
+            <div className='border-l border-gray-300 '></div>
+            <div>Any Time</div>
+            <div className='border-l border-gray-300 '></div>
+            <div>Any One</div>
+            <button className='bg-primary text-white rounded-full p-1'>
+              <SearchIcon />
+            </button>
+          </div>
+        </div>
+        <div className='flex items-center gap-10'>
+          <p>lifella your home</p>
+          <GlobeIcon />
+          <div className='flex justify-between border border-gray-400 rounded-full p-1 gap-3 px-3'>
+            <BarIcon />
+
+            <UserIcon />
+          </div>
         </div>
       </header>
     </div>
--- src/assets/Icons.jsx
+++ src/assets/Icons.jsx
@@ -24,12 +24,60 @@ export const SearchIcon = () => (
     viewBox='0 0 24 24'
     strokeWidth={1.5}
     stroke='currentColor'
-    className='w-6 h-6'
+    className='w-4 h-4 '
   >
     <path
       strokeLinecap='round'
       strokeLinejoin='round'
       d='m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z'
+    />
+  </svg>
+)
+
+export const GlobeIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    fill='none'
+    viewBox='0 0 24 24'
+    strokeWidth={1.5}
+    stroke='currentColor'
+    className='w-6 h-6'
+  >
+    <path
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      d='M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418'
+    />
+  </svg>
+)
+
+export const BarIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    fill='none'
+    viewBox='0 0 24 24'
+    strokeWidth={1.5}
+    stroke='currentColor'
+    className='w-6 h-6'
+  >
+    <path
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
+    />
+  </svg>
+)
+export const UserIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    viewBox='0 0 24 24'
+    fill='currentColor'
+    className='w-6 h-6'
+  >
+    <path
+      fillRule='evenodd'
+      d='M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z'
+      clipRule='evenodd'
     />
   </svg>
 )
